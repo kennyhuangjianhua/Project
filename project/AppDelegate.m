8635f3d643e4c167cfc83aab6cb9ac75fb07b8c5
黄建华 <119541779@qq.com>
2017-11-16 10:54:43 +0800

    1

--- project/AppDelegate.m
+++ project/AppDelegate.m
@@ -30,28 +30,6 @@
     return YES;
 }
 
-- (void)loadSpeakSdk
-{
-//    //显示SDK的版本号
-//    NSLog(@"verson=%@",[IFlySetting getVersion]);
-//    
-//    //设置sdk的log等级，log保存在下面设置的工作路径中
-//    [IFlySetting setLogFile:LVL_ALL];
-//    
-//    //打开输出在console的log开关
-//    [IFlySetting showLogcat:NO];
-//    
-//    //设置sdk的工作路径
-//    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
-//    NSString *cachePath = [paths objectAtIndex:0];
-//    [IFlySetting setLogFilePath:cachePath];
-//    
-//    //创建语音配置,appid必须要传入，仅执行一次则可
-//    NSString *initString = [[NSString alloc] initWithFormat:@"appid=%@",@"5806eaa3"];
-//    
-//    //所有服务启动前，需要确保执行createUtility
-//    [IFlySpeechUtility createUtility:initString];
-}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
